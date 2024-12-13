8581f54d0277b80973bc75bd21e629f157767a43
Akshay Pethani <akshay.pethani@indexnine.com>
2024-12-13 19:53:02 +0530

    Just works

--- wifi-login.bat
+++ wifi-login.bat
@@ -2,27 +2,27 @@
 setlocal enabledelayedexpansion
 
 :: Set the SSID of the WiFi network
-set SSID=91springboard
+set "SSID=91springboard"
 
 :: Set the path for the credentials file
-set SBC_FOLDER=%USERPROFILE%\.sbc
-set CREDENTIALS_FILE=%SBC_FOLDER%\credentials.txt
+set "SBC_FOLDER=%USERPROFILE%\.sbc"
+set "CREDENTIALS_FILE=%SBC_FOLDER%\credentials.txt"
 
 :: Ensure the directory exists
 if not exist "%SBC_FOLDER%" (
     mkdir "%SBC_FOLDER%"
     if %errorlevel% neq 0 (
-        echo Failed to create directory %SBC_FOLDER%.
+        echo Failed to create directory for credentials: %SBC_FOLDER%.
         exit /b 1
     )
 )
 
 :: Function to connect to WiFi
 :connect_to_wifi
-netsh wlan connect name=%SSID%
+netsh wlan connect name="%SSID%"
 if %errorlevel% neq 0 (
- echo Failed to connect to WiFi network %SSID%.
- exit /b 1
+    echo Failed to connect to WiFi network %SSID%.
+    exit /b 1
 )
 echo Successfully connected to %SSID%.
 goto :login
@@ -30,29 +30,32 @@ goto :login
 :: Function to login to the portal
 :login
 if exist "%CREDENTIALS_FILE%" (
- for /f "tokens=1,2" %%i in (%CREDENTIALS_FILE%) do (
-  set WIFI_USER=%%i
-  set PASSWORD=%%j
- )
-) else ( 
- set /p WIFI_USER=Enter Username:
- set /p PASSWORD=Enter Password:
- echo %WIFI_USER% %PASSWORD% > "%CREDENTIALS_FILE%"
-)
-        set PASSWORD=%%j
+
+    for /f "tokens=1,2" %%i in (%CREDENTIALS_FILE%) do (
+        set "WIFI_USER=%%i"
+        set "PASSWORD=%%j"
     )
 ) else (
-    set /p WIFI_USER=Enter Username:
-    set /p PASSWORD=Enter Password:
-    echo %WIFI_USER% %PASSWORD% > "%CREDENTIALS_FILE%"
+    set /p "WIFI_USER=Enter Username: "
+    set /p "PASSWORD=Enter Password: "
+    echo !WIFI_USER! !PASSWORD!> "%CREDENTIALS_FILE%"
 )
 
-:: HTTP API call
-set URL=http://example.com/api/login
-set POST_DATA="username=%WIFI_USER%&password=%PASSWORD%"
+echo Username: !WIFI_USER!
+echo Password: !PASSWORD!
 
-:: Use curl for HTTP POST request
-curl -X POST -d %POST_DATA% %URL%
+:: HTTP API call
+
+curl POST ^
+  "https://portal.91springboard.com/login" ^
+  --data-urlencode "username=!WIFI_USER!" ^
+  --data-urlencode "password=!PASSWORD!" ^
+  -H "Content-Type: application/x-www-form-urlencoded" ^
+  -H "Accept: text/html,application/xhtml+xml" ^
+  --verbose ^
+  --location
+
+
 
 if %errorlevel% neq 0 (
     echo Login failed.
